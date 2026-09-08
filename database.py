d5ca47c33a8d037e1bd57116a76f955148ef16c3
HESLO <evil2000devil@gmail.com>
2026-09-08 20:08:57 +0330

    added the other tables

--- database.py
+++ database.py
@@ -2,30 +2,71 @@ import sqlite3
 
 connection = sqlite3.connect("ecommerce.db")
 
+connection.execute("PRAGMA foreign_keys = ON")
+
 cursor = connection.cursor()
+
 cursor.execute("""
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     phone TEXT,
-    created_at TEXT
+    created_at DATE NOT NULL
 )
 """)
 
 cursor.execute("""
-CREATE TABLE categories (
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-description TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
 )
 """)
 
 cursor.execute("""
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY,
+    category_id INTEGER,
+    name TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL,
+    created_at DATE NOT NULL,
 
+    FOREIGN KEY (category_id)
+    REFERENCES categories(id)
+)
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    status TEXT NOT NULL,
+    total_amount INTEGER NOT NULL,
+    created_at DATE NOT NULL,
+
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS order_items (
+    id INTEGER PRIMARY KEY,
+    order_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER NOT NULL,
+    price_at_purchase INTEGER NOT NULL,
+
+    FOREIGN KEY (order_id)
+    REFERENCES orders(id),
+
+    FOREIGN KEY (product_id)
+    REFERENCES products(id)
+)
+""")
 
 connection.commit()
 
